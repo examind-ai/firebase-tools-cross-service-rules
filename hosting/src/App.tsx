a64a1f204993f13d62e3df388547ab0558c33917
Johnny Oshika <johnnyoshika@gmail.com>
2022-12-05 11:57:40 -0800

    Setup demo

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  User,
+} from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { getDownloadURL, ref } from 'firebase/storage';
+import { useEffect, useState } from 'react';
+import './App.css';
+import { auth, db, storage } from './firebase';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, user => {
+        setUser(user);
+        if (user)
+          setDoc(doc(db, 'courses/course1'), {
+            users: { [user.uid]: { exists: true } },
+          });
+      }),
+    [setUser],
+  );
+
+  const handleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+  };
+
+  const handleDownload = async () => {
+    const fileRef = ref(storage, 'courses/course1/file.txt');
+    const url = await getDownloadURL(fileRef);
+    const response = await fetch(url);
+    console.log('response', response);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {user ? (
+        <div>
+          <div>Welcome {user.email}</div>
+          <div>UID: {user.uid}</div>
+          <div>
+            <button type="button" onClick={handleDownload}>
+              Download
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button type="button" onClick={handleSignIn}>
+            Sign in
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
