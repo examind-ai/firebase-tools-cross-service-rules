2af68b07df15b796d78174314f813d4102e75420
Johnny Oshika <johnnyoshika@gmail.com>
2022-12-09 10:18:00 -0800

    Add null field in Firestore object to reproduce bug

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -5,13 +5,18 @@ import {
   User,
 } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
-import { getDownloadURL, ref } from 'firebase/storage';
+import {
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
 import { useEffect, useState } from 'react';
 import './App.css';
 import { auth, db, storage } from './firebase';
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [objectName, setObjectName] = useState<string>();
 
   useEffect(
     () =>
@@ -20,6 +25,7 @@ function App() {
         if (user)
           setDoc(doc(db, 'courses/course1'), {
             users: { [user.uid]: { exists: true } },
+            deletedAt: null,
           });
       }),
     [setUser],
@@ -31,10 +37,28 @@ function App() {
   };
 
   const handleDownload = async () => {
-    const fileRef = ref(storage, 'courses/course1/file.txt');
+    const fileRef = ref(storage, objectName);
     const url = await getDownloadURL(fileRef);
     const response = await fetch(url);
     console.log('response', response);
+  };
+
+  const handleUpload = (e: any) => {
+    const file = e.target.files[0];
+    const objectName = `courses/course1/${file.name}`;
+    const storageRef = ref(storage, objectName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      _snapshot => {},
+      error => {
+        alert(error.message);
+      },
+      () => {
+        setObjectName(objectName);
+      },
+    );
   };
 
   return (
@@ -42,15 +66,27 @@ function App() {
       {user ? (
         <div>
           <div>Welcome {user.email}</div>
-          <div>UID: {user.uid}</div>
-          <div>
-            <button type="button" onClick={handleDownload}>
-              Download
-            </button>
+          <div style={{ marginTop: '24px' }}>
+            <p>
+              To reproduce bug, upload a file then try to download it
+              again.
+            </p>
+            {objectName ? (
+              <div>
+                <button type="button" onClick={handleDownload}>
+                  Download
+                </button>
+              </div>
+            ) : (
+              <div>
+                <input type="file" onChange={handleUpload} />
+              </div>
+            )}
           </div>
         </div>
       ) : (
         <div>
+          <p>First sign in with any account:</p>
           <button type="button" onClick={handleSignIn}>
             Sign in
           </button>
